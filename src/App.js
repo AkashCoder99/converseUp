4bbd852a791b3cde2a5c940b6c25330f71cd09da
Punit Bathija <punit644@gmail.com>
2023-02-02 13:35:57 +0530

    Building home page using diferrent components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import Register from "./Pages/Register";
-import Login from "./Pages/Login";
+// import Register from "./Pages/Register";
+// import Login from "./Pages/Login";
+import Home from "./Pages/Home";
 import "./Style.scss";
 
 function App() {
   return (
     <div className="App">
-      <Register />
-      <Login />
+      {/* <Register /> */}
+      {/* <Login /> */}
+      <Home />
     </div>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Chat from "../Components/Chat";
+import Sidebar from "../Components/Sidebar";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <h1>This is my home component</h1>
+      <Sidebar />
+      <Chat />
+    </div>
+  );
 };
 
 export default Home;
