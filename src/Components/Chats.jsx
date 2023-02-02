349701a63cf28d68160a2b6c35e4f14b0a93b112
Punit Bathija <punit644@gmail.com>
2023-02-02 14:45:26 +0530

    Completing design for my sidebar

--- src/Components/Chats.jsx
+++ src/Components/Chats.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Chats = () => {
-  return <div>Chats</div>;
+  return (
+    <div className="chats">
+      <div className="search-results">
+        <img
+          className="profile"
+          src="https://pbs.twimg.com/profile_images/1590363423011110912/x0jZMgC5_400x400.jpg"
+          alt="profile"
+        />
+        <span>Punit Bathija</span>
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Search from "./Search";
+import Chats from "./Chats";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <Navbar />
       <Search />
-      Sidebar
+      <Chats />
+      <Chats />
+      <Chats />
+      <Chats />
     </div>
   );
 };
