b043291617b941dde28b0282c4b59aac37aaab53
Punit Bathija <punit644@gmail.com>
2023-02-05 11:57:39 +0530

    Wrote a function to check all the  users in userChats collection

--- src/Components/Chats.jsx
+++ src/Components/Chats.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../Context/AuthContext";
+import { db } from "../firebase.config";
 
 const Chats = () => {
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+        console.log(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
   return (
     <div className="chats">
       <div className="search-results">
