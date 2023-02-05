16b25905a689f8b44de22943219cb8a8e9b9fc3e
Punit Bathija <punit644@gmail.com>
2023-02-05 11:30:12 +0530

    FIguring out how to search and add a user to db for individual chats collection

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import { db } from "../firebase.config";
-import { query, collection, where, getDocs } from "firebase/firestore";
+import { query, collection, where, getDocs, setDoc } from "firebase/firestore";
+import { AuthContext } from "../Context/AuthContext";
 
 const Search = () => {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState(null);
   const [err, setErr] = useState(false);
+  const { currentUser } = useContext(AuthContext);
+
+  const handleSelect = async () => {
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const userRef = await getDocs(db, "chats", combinedId);
+      if (!userRef.exists()) {
+        await setDoc(doc, (db, "chats", combinedId), { messages: [] });
+      }
+    } catch (err) {}
+  };
 
   const handleSearch = async () => {
     const q = query(
@@ -41,11 +56,11 @@ const Search = () => {
       </div>
       {err && (
         <span style={{ textAling: "center", color: "white" }}>
-          User not found
+          User not found!
         </span>
       )}
       {user && (
-        <div className="search-results">
+        <div className="search-results" onClick={handleSelect}>
           <img className="profile" src={user.photoURL} alt="profile" />
           <span>{user.displayName}</span>
         </div>
