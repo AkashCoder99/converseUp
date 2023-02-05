00b39369356d9b12280af4db20025e5971ff1741
Punit Bathija <punit644@gmail.com>
2023-02-05 11:08:20 +0530

    Fixed the error where we could not find a user

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -2,47 +2,48 @@ import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import { db } from "../firebase.config";
 import { query, collection, where, getDocs } from "firebase/firestore";
-import ErrorIcon from "@mui/icons-material/Error";
 
 const Search = () => {
-  const [userName, setUserName] = useState("");
+  const [username, setUsername] = useState("");
   const [user, setUser] = useState(null);
   const [err, setErr] = useState(false);
-  const handleSearch = async (e) => {
-    e.preventDefault();
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+
     try {
-      const q = query(
-        collection(db, "users"),
-        where("displayName", "==", userName)
-      );
       const querySnapshot = await getDocs(q);
       querySnapshot.forEach((doc) => {
         setUser(doc.data());
       });
-    } catch (error) {
+    } catch (err) {
       setErr(true);
-      {
-        err && (
-          <span style={{ color: "black", textAlign: "center" }}>
-            <ErrorIcon color="error" />
-            Something went wrong...
-          </span>
-        );
-      }
     }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
   };
 
   return (
     <div className="search">
-      <form className="search-form">
+      <div className="search-form">
         <SearchIcon color="action" />
         <input
           type="text"
+          onChange={(e) => setUsername(e.target.value)}
           placeholder="search for a user"
-          onChange={(e) => setUserName(e.target.value)}
-          onSubmit={handleSearch}
+          onKeyDown={handleKey}
         />
-      </form>
+      </div>
+      {err && (
+        <span style={{ textAling: "center", color: "white" }}>
+          User not found
+        </span>
+      )}
       {user && (
         <div className="search-results">
           <img className="profile" src={user.photoURL} alt="profile" />
